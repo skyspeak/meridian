9a2c91e0ca2d9483117c5a90ac9d62de931eb76c
Megh Gautam <skyspeak@gmail.com>
2025-07-10 22:27:16 -0700

    Fix missing dependencies - add uuid and @types/uuid

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,18 +1,1 @@
-self.__BUILD_MANIFEST = {
-  "polyfillFiles": [
-    "static/chunks/polyfills.js"
-  ],
-  "devFiles": [],
-  "ampDevFiles": [],
-  "lowPriorityFiles": [],
-  "rootMainFiles": [],
-  "pages": {
-    "/_app": []
-  },
-  "ampFirstPages": []
-};
-self.__BUILD_MANIFEST.lowPriorityFiles = [
-"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
-,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
-
-];
+self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-41a9ee0a1d65a3d2.js","static/chunks/fd9d1056-cce117dc4e21e608.js","static/chunks/117-33e717f2335c9cff.js","static/chunks/main-app-519c2766417cb8e2.js"],pages:{"/_app":["static/chunks/webpack-41a9ee0a1d65a3d2.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-ad77119212d2cb66.js","static/chunks/pages/_app-72b849fbd24ac258.js"],"/_error":["static/chunks/webpack-41a9ee0a1d65a3d2.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-ad77119212d2cb66.js","static/chunks/pages/_error-7ba65e1336b92748.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
